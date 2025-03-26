5ac07c3ecbd3d83961eea045f5cfeb9e3289e293
DubzzzJohan <dubzzzstepss@gmail.com>
2025-03-26 19:52:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    private static object username;
+
+    public static void Main() 
+    {
+        //Ask for the user's name
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        // Store user input
+        // Greet the user
+        Console.WriteLine($"Hello, {username}! Welcome to C#.");
+        // Keep console open until user presses a key
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }  
+}
